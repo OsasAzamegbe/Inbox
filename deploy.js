829c553a8e181f21279fa66b3a45755ac8af69e8
Osamudiamen <osas.azamegbe@gmail.com>
2022-02-02 23:10:16 +0000

    deploy contract

---
+++ deploy.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const HdWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const deploy = async () => {
+    const accountProvider = new HdWalletProvider(
+        process.env.ACCOUNT_MNEMONIC,
+        process.env.INFURA_RINKEBY_ENDPOINT
+    );
+    const web3 = new Web3(accountProvider);
+    console.log('Getting accounts from web3 provider ...');
+
+    const accounts = await web3.eth.getAccounts();
+    console.log('Deploying contract with account', accounts[0]);
+
+    const deployTxnObj = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ["Default message"] })
+        .send({ from: accounts[0], gas: '1000000' });
+    console.log('Contract deployed to', deployTxnObj.options.address);
+
+    accountProvider.engine.stop();
+}
+
+deploy();
